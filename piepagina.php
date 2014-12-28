2968ccfe2234eaad9e67513ffa8d260b0a94822a
yukumura <yukumuradota@gmail.com>
2014-12-28 14:42:25 +0000

    [prova database]

--- piepagina.php
+++ piepagina.php
@@ -4,7 +4,7 @@
   <?php
   include('cuore.php');
   
-  $query="SELECT inseriti FROM iscritti where username = '$_SESSION[username]'";
+  $query="SELECT inseriti FROM iscritti where username = '$_SESSION [username]'";
   $result = $mysqli->query($query);
   $row = mysqli_fetch_row($result);
   
