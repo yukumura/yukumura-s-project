642b4f4967d708dd6a3690f5caec6bab5f5b3ada
yukumura <yukumuradota@gmail.com>
2014-12-28 16:55:28 +0000

    [prova database]

---
+++ database_connect.php
@@ -0,0 +1,16 @@
+<?php
+$ip = "localhost";
+$user = "cicilloniAlberto";
+$password = "scimmia454";
+$db = "amm14_cicilloniAlberto";
+$mysqli = new mysqli();
+$mysqli->connect($ip, $user, $password, $db);
+// verifico la presenza di errori
+if($mysqli->connect_errno!= 0){
+// gestione errore
+$idErrore= $mysqli->connect_errno;
+$msg= $mysqli->connect_error;
+error_log("Errore nella connessione al server $idErrore: $msg", 0);
+echo "Errore nella connessione $msg";
+}
+?>
